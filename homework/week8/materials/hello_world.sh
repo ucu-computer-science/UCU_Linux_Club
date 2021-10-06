d79c27dc00b424b9e838ebd03b5e432e410bf63b
LastGenius-edu <as.andreysultanov@gmail.com>
2021-10-06 09:40:02 +0300

    Update and clean up homeworks

--- homework/week8/materials/hello_world.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# remember that on your device path will be another
-echo "Started. Date: $(date)" >> /home/pasha/Documents/Linux_club/UCU_Linux_Club/weeks/week8/materials/hello_world.log
-while true
-do
-	echo "Hello, world"
-	sleep 1
-done
